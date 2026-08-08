c7301ea3be75ad76592e3d6e89338c2488a2fd4d
Harshgg1 <hhggleet@gmail.com>
2026-08-08 13:29:51 +0530

    Fix bug and now cloning pipeline is working

--- apps/worker/src/config/path.ts
+++ apps/worker/src/config/path.ts
@@ -1,7 +1,16 @@
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const PROJECT_ROOT = path.resolve(
+  __dirname,
+  "../../../.."
+);
 
 export const REPOSITORY_ROOT = path.join(
-    process.cwd(),
-    "tmp",
-    "repositories"
+  PROJECT_ROOT,
+  "tmp",
+  "repositories"
 );
